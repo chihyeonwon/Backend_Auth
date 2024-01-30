f8ab1e494d2365f9c4f10f3673626d2b3b5e2918
ylose <heungeob1003@gmail.com>
2024-01-30 14:19:30 +0900

    create 메서드 작성

--- demo/src/main/java/com/example/demo/service/TodoService.java
+++ demo/src/main/java/com/example/demo/service/TodoService.java
@@ -1,11 +1,16 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.model.TodoEntity;
 import com.example.demo.persistence.TodoRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class TodoService {
 	
@@ -20,6 +25,25 @@ public class TodoService {
 		// TodoEntity °Ë»ö
 		TodoEntity savedEntity = repository.findById(entity.getId()).get();
 		return savedEntity.getTitle();
+	}
+	
+	public List<TodoEntity> create(final TodoEntity entity){
+		// Validations
+		if(entity == null) {
+			log.warn("Entity cannot be null");
+			throw new RuntimeException("Entity cannot be null");
+		}
+		
+		if(entity.getUserId() == null) {
+			log.warn("Unknown user.");
+			throw new RuntimeException("Unknown user.");
+		}
+		
+		repository.save(entity);
+		
+		log.info("Entity Id : {} is saved.", entity.getId());
+		
+		return repository.findByUserId(entity.getUserId());
 	}
 	
 }
