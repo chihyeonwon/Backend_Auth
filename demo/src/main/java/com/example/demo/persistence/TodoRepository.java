5aa9153337283fc7036c656fc8a0b1584e7c511d
ylose <heungeob1003@gmail.com>
2024-01-30 14:18:11 +0900

    findByUserId 메서드 추가

--- demo/src/main/java/com/example/demo/persistence/TodoRepository.java
+++ demo/src/main/java/com/example/demo/persistence/TodoRepository.java
@@ -1,9 +1,12 @@
 package com.example.demo.persistence;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.model.TodoEntity;
 
 public interface TodoRepository extends JpaRepository<TodoEntity, String>{
 	
+	List<TodoEntity> findByUserId(String userId);
 }
