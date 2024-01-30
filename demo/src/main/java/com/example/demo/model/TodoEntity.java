4d89d33b82d191a44883209bc1706ed41d3a8370
ylose <heungeob1003@gmail.com>
2024-01-30 13:58:12 +0900

    @Id, @GeneratedValue, @GenericGenerator 추가

--- demo/src/main/java/com/example/demo/model/TodoEntity.java
+++ demo/src/main/java/com/example/demo/model/TodoEntity.java
@@ -1,7 +1,11 @@
 package com.example.demo.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,6 +19,10 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name="Todo")
 public class TodoEntity {
+	
+	@Id
+	@GeneratedValue(generator="system-uuid")
+	@GenericGenerator(name="system-uuid", strategy="uuid")
 	private String id; // 이 오브젝트의 아이디
 	private String userId; // 이 오브젝트를 생성한 유저의 아이디
 	private String title; // Todo 타이틀 예) 운동하기
