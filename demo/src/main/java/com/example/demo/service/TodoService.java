2bcdfd4d0bb8533a94d12733b730a7dfe7335ed5
ylose <heungeob1003@gmail.com>
2024-01-31 14:14:23 +0900

    Todo를 업데이트 하기 위한 update 메서드 추가

--- demo/src/main/java/com/example/demo/service/TodoService.java
+++ demo/src/main/java/com/example/demo/service/TodoService.java
@@ -1,7 +1,7 @@
 package com.example.demo.service;
 
-import java.security.PublicKey;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -56,4 +56,24 @@ public class TodoService {
 		return repository.findByUserId(userId);
 	}
 	
+	public List<TodoEntity> update(final TodoEntity entity){
+		// (1) 저장할 엔티티가 유효한지 확인한다.
+		validate(entity);
+		
+		// (2) 넘겨받은 엔티티 id를 이용해 TodoEntity를 가져온다.
+		final Optional<TodoEntity> original = repository.findById(entity.getId());
+		
+		if(original.isPresent()) {
+			// (3) 반환된 TodoEntity가 존재하면 값을 새 entity의 값으로 덮어 씌운다.
+			final TodoEntity todo = original.get();
+			todo.setTitle(entity.getTitle());
+			todo.setDone(entity.isDone());
+			
+			// (4) 데이터베이스에 새 값을 저장한다.
+			repository.save(todo);
+		}
+		
+		// 유저의 모든 Todo 리스트를 반환한다.
+		return retrieve(entity.getUserId());
+	}
 }
