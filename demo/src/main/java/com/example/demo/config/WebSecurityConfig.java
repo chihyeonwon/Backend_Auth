99cca69279ddc4a4c761856405f56c8ba98e41fd
ylose <heungeob1003@gmail.com>
2024-02-16 15:04:54 +0900

    WebSecurityConfig 작성

---
+++ demo/src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -0,0 +1,39 @@
+package com.example.demo.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
+
+import com.example.demo.security.JwtAuthenticationFilter;
+
+import lombok.extern.slf4j.Slf4j;
+
+@EnableWebSecurity
+@Slf4j
+public class WebSecurityConfig {
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .cors()
+            .and()
+            .csrf().disable()
+            .httpBasic().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .authorizeRequests()
+            .antMatchers("/", "/auth/**").permitAll()
+            .anyRequest().authenticated()
+            .and()
+            .addFilterAfter(jwtAuthenticationFilter, CorsFilter.class);
+
+        return http.build();
+    }
+}
